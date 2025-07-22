b7abb0602f01d8422b177dad91f7cbfde7b45696
Pressense Fasani <camillefasani@gmail.com>
2025-07-22 10:33:37 +0200

    add: start authentication app

--- pipou_blog/pipou_blog/settings.py
+++ pipou_blog/pipou_blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 

39e0842421de8054cb67e98d0f4404c456f9bc67
Pressense Fasani <camillefasani@gmail.com>
2025-07-22 11:27:20 +0200

    add: create and migrate User model

--- pipou_blog/authentication/models.py
+++ pipou_blog/authentication/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        null=True,
+        blank=True,
+        verbose_name='Photo de profil'
+    )
+    
+    date_updated = models.DateTimeField(auto_now=True, verbose_name='Date de mise à jour')
+    biography = models.TextField(max_length=500, null=True, blank=True, verbose_name='Biographie')
+    email = models.EmailField(unique=True)
+
+    # Annule le champ par défaut username
+    username = None
+
+    # Indique que le champ email sera utilisé pour  l'authentification
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    # Personnalisation de l'affichage dans l'admin 
+    class Meta:
+        verbose_name = 'Utilisateur-rice'
+        verbose_name_plural = 'Utilisateur-rices'
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
--- pipou_blog/pipou_blog/settings.py
+++ pipou_blog/pipou_blog/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'authentication.User'
+
+# Pour gérer les fichiers média (images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

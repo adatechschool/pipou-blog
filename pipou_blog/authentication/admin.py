ebe20f192225108dff1c13b5821e38d7765b18b3
Pressense Fasani <camillefasani@gmail.com>
2025-07-23 11:19:23 +0200

    fix: urls.py

--- pipou_blog/authentication/admin.py
+++ pipou_blog/authentication/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'date_joined','biography')
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
 
-admin.site.register(User, UserAdmin)
+    list_display = ('email', 'first_name', 'last_name', 'date_joined')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Informations personnelles"), {"fields": ("first_name", "last_name", "biography", "profile_picture")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Dates importantes"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "first_name", "last_name"),
+        }),
+    )
+
+    search_fields = ("email", "first_name", "last_name")

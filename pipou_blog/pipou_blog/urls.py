6e1cfb4b6a3a2523b46430109a2e97046d989b31
Pressense Fasani <camillefasani@gmail.com>
2025-07-23 12:03:45 +0200

    add: logout feature

--- pipou_blog/pipou_blog/urls.py
+++ pipou_blog/pipou_blog/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from blog import views
-# from authentication import views
-import blog.views
+
 import authentication.views
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='index'),
-    path('login/', authentication.views.login_page, name="login"),
+    path('login/', authentication.views.login_page, name='login'),
+    path('logout/', authentication.views.logout_user, name='logout'),
 ]
